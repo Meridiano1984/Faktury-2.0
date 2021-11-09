1f1c68bbe749221a408477d15d9f2f23a2ad51e3
Meridiano1984 <259125@student.pwr.edu.pl>
2021-11-09 12:43:54 +0100

    "new databse structure and new data to added to DB"

--- src/main/java/DataBase/DatabaseInitialization.java
+++ src/main/java/DataBase/DatabaseInitialization.java
@@ -1,83 +1,10 @@
 package DataBase;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-
 import static DataBase.QueryExecutor.executeQuery;
 
 public class DatabaseInitialization {
 
     public DatabaseInitialization() {
-    }
-
-    public void tworzenieBazydanych (){
-
-        String Query;
-
-        try {
-             Query = """
-                    CREATE TABLE faktury (\s
-                     faktura_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     nr_faktury VARCHAR(15),
-                     data_wystawienia DATE,
-                     cenaBrutto FLOAT(2)
-                     );
-                            """;
-
-            executeQuery(Query);
-        }catch (RuntimeException e){
-            System.out.println("TABELA FAKTURY JUZ ISTNIEJE");
-        }
-
-
-        try {
-            Query = """
-                     CREATE TABLE kontrachenci (\s
-                     kontrachent_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     kontrachent_name VARCHAR(40),
-                     nip VARCHAR(15)
-                     );
-                    """;
-
-            executeQuery(Query);
-        }catch (RuntimeException e){
-            System.out.println("Tabela KONTRACHENCI JUZ ISTNIEJE");
-        }
-
-        try {
-            Query = """ 
-                     CREATE TABLE produkty ( 
-                     produkt_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     produkt_name VARCHAR(40),
-                     cena_brutto FLOAT(20)
-                     );
-                    """;
-
-            executeQuery(Query);
-        } catch (RuntimeException e){
-            System.out.println("Tabela produkty juz istnieje");
-        }
-
-
-        //TODO ZAPROJEKTOWAC BAZEDANYCH TAK ZE KAZDA FAKTURA MA JEDNEGO KONTRACHENTA (faktura i kontrachent ot primary key) I WIELE PRODUKTOW COS JAKBY TABLICA PRODUKTOW
-
-
-            Query = """
-                    CREATE TABLE wystawione_faktury ( 
-                     faktura_id INT,
-                     kontrachent_id INT,
-                     produkt_id INT,
-                     ilosc_produktow INT,
-                     PRIMARY KEY(faktura_id, produkt_id),
-                     FOREIGN KEY(faktura_id) REFERENCES faktury(faktura_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                     FOREIGN KEY(kontrachent_id) REFERENCES kontrachenci(kontrachent_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                     FOREIGN KEY(produkt_id) REFERENCES produkty(produkt_id) ON DELETE CASCADE ON UPDATE CASCADE
-                     );
-                    """;
-            executeQuery(Query);
-
-
     }
 
     public void tworzenieBazydanychV2 (){
@@ -89,26 +16,6 @@ public class DatabaseInitialization {
         /**
          * MAKSYMALNA WARTOSC SPRZEDAZY DO WYSTAWIANIA NA FAKTURZE TO (10 000 000 000 - 1)
          */
-
-        try {
-            Query = """
-                    CREATE TABLE faktury (\s
-                     faktura_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     typ_faktury VARCHAR(15),
-                     nr_faktury VARCHAR(15),
-                     data_wystawienia DATE,                    
-                     kontrachent_id INT,
-                     wartosc_sprzedazy_netto DOUBLE(9,2),
-                     wartosc_sprzedazy_brutto DOUBLE(9,2),
-                     wartosc_calkowita_podatku DOUBLE(9,2),
-                     uwagi TINYTEXT
-                     );
-                            """;
-
-            executeQuery(Query);
-        }catch (RuntimeException e){
-            System.out.println("TABELA FAKTURY JUZ ISTNIEJE");
-        }
 
 
         try {
@@ -131,6 +38,27 @@ public class DatabaseInitialization {
         }
 
         try {
+            Query = """
+                    CREATE TABLE faktury (\s
+                     faktura_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     typ_faktury VARCHAR(15),
+                     nr_faktury VARCHAR(15),
+                     data_wystawienia DATE,                    
+                     kontrachent_id INT,
+                     wartosc_sprzedazy_netto DOUBLE(9,2),
+                     wartosc_sprzedazy_brutto DOUBLE(9,2),
+                     wartosc_calkowita_podatku DOUBLE(9,2),
+                     uwagi TINYTEXT,
+                     FOREIGN KEY(kontrachent_id) REFERENCES kontrachenci(kontrachent_id) ON DELETE SET NULL                     
+                     );
+                            """;
+
+            executeQuery(Query);
+        }catch (RuntimeException e){
+            System.out.println("TABELA FAKTURY JUZ ISTNIEJE");
+        }
+
+        try {
             Query = """ 
                      CREATE TABLE towary ( 
                      produkt_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -141,7 +69,7 @@ public class DatabaseInitialization {
                      stawka_VAT_zakupu INT(2),
                      cena_sprzedazy_netto DOUBLE(9,2),
                      cena_sprzedazy_brutto DOUBLE(9,2),
-                     stawka_VAT_sprzedazy INT(2),                  
+                     stawka_VAT_sprzedazy INT(2)                  
                      );
                     """;
 
@@ -157,7 +85,6 @@ public class DatabaseInitialization {
         Query = """
                     CREATE TABLE produkty_na_fakturach_vat ( 
                      faktura_id INT,
-                     kontrachent_id INT,
                      produkt_id INT,
                      ilosc_produktow INT,
                      wartosc_netto DOUBLE(9,2),
@@ -165,8 +92,7 @@ public class DatabaseInitialization {
                      wartosc_vat DOUBLE(9,2),
                      PRIMARY KEY(faktura_id, produkt_id),
                      FOREIGN KEY(faktura_id) REFERENCES faktury(faktura_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                     FOREIGN KEY(kontrachent_id) REFERENCES kontrachenci(kontrachent_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                     FOREIGN KEY(produkt_id) REFERENCES produkty(produkt_id) ON DELETE CASCADE ON UPDATE CASCADE
+                     FOREIGN KEY(produkt_id) REFERENCES towary(produkt_id) ON DELETE CASCADE ON UPDATE CASCADE
                      );
                     """;
         executeQuery(Query);
@@ -174,17 +100,16 @@ public class DatabaseInitialization {
 
     }
 
-
     public void dodaniePrzykładowychdanych(){
 
         try {
-            executeQuery("INSERT INTO kontrachenci VALUES(1,'Biedronka',1234567890);");
-            executeQuery("INSERT INTO kontrachenci VALUES(2,'Autocentrum',2134567890);");
-            executeQuery("INSERT INTO kontrachenci VALUES(3,'PGE-Belchatow',3354618291);");
-            executeQuery("INSERT INTO kontrachenci VALUES(4,'Kamilex',3466789008);");
-            executeQuery("INSERT INTO kontrachenci VALUES(5,'II LO Radomsko',1111144444);");
-            executeQuery("INSERT INTO kontrachenci VALUES(6,'Fitnes Centrum',3334445559);");
-            executeQuery("INSERT INTO kontrachenci VALUES(7,'Riff',1231231234);");
+            executeQuery("INSERT INTO kontrachenci VALUES(1,'KRAJOWY','Biedronka',1234567890,'Polska','Wrocław','Swidnicka','12c/32');");
+            executeQuery("INSERT INTO kontrachenci VALUES(2,'KRAJOWY','Autocentrum',2134567890,'Polska','Wrocław','Swidnicka','12c/32');");
+            executeQuery("INSERT INTO kontrachenci VALUES(3,'KRAJOWY','PGE-Belchatow',3354618291,'Polska','Wrocław','Swidnicka','12c/32');");
+            executeQuery("INSERT INTO kontrachenci VALUES(4,'KRAJOWY','Kamilex',3466789008,'Polska','Wrocław','Swidnicka','12c/32');");
+            executeQuery("INSERT INTO kontrachenci VALUES(5,'KRAJOWY','II LO Radomsko',1111144444,'Polska','Wrocław','Swidnicka','12c/32');");
+            executeQuery("INSERT INTO kontrachenci VALUES(6,'KRAJOWY','Fitnes Centrum',3334445559,'Polska','Wrocław','Swidnicka','12c/32');");
+            executeQuery("INSERT INTO kontrachenci VALUES(7,'KRAJOWY','Riff',1231231234,'Polska','Wrocław','Swidnicka','12c/32');");
 
             System.out.println("Dodano juz kontrachentow");
         }catch (RuntimeException e ){
@@ -192,130 +117,51 @@ public class DatabaseInitialization {
         }
 
         try {
-            executeQuery("INSERT INTO produkty VALUES(1,'Mleko',2.50);");
-            executeQuery("INSERT INTO produkty VALUES(2,'Laptop',2500)");
-            executeQuery("INSERT INTO produkty VALUES(3,'Jeansy',250);");
-            executeQuery("INSERT INTO produkty VALUES(4,'Windows',4000);");
-            executeQuery("INSERT INTO produkty VALUES(5,'Bulka',0.39);");
-            executeQuery("INSERT INTO produkty VALUES(6,'Margherita',12.50);");
+            executeQuery("INSERT INTO towary VALUES(1,'Mleko','szt',2.50,3.50,23,4.50,5.60,23);");
+            executeQuery("INSERT INTO towary VALUES(2,'Laptop','szt',2500,3000,23,3300,3600,23)");
+            executeQuery("INSERT INTO towary VALUES(3,'Jeansy','szt',250,300,23,320,360,34);");
+            executeQuery("INSERT INTO towary VALUES(4,'Windows','szt',4000,5000,23,5500,6000,20);");
+            executeQuery("INSERT INTO towary VALUES(5,'Bulka','szt',0.39,0.60,23,1.20,1.80,23);");
+            executeQuery("INSERT INTO towary VALUES(6,'Margherita','szt',12.50,15,23,18,21,23);");
 
             System.out.println("Dodano produkty");
         }catch (RuntimeException e ){
             e.printStackTrace();
         }
 
-            executeQuery("INSERT INTO faktury VALUES(1,'1/10/2021','2021-12-23',1,0);");
-            executeQuery("INSERT INTO faktury VALUES(2,'2/10/2021','2022-01-01',2,0);");
-            executeQuery("INSERT INTO faktury VALUES(3,'3/10/2021','2022-01-02',3,0);");
+            executeQuery("INSERT INTO faktury VALUES(1,'VAT','1/10/2021','2021-12-23',1,0,0,0,'pierwsza faktura');");
+            executeQuery("INSERT INTO faktury VALUES(2,'VAT','2/10/2021','2022-01-01',2,0,0,0,'pierwsza faktura');");
+            executeQuery("INSERT INTO faktury VALUES(3,'VAT','3/10/2021','2022-01-02',3,0,0,0,'pierwsza faktura');");
 
 
             System.out.println("Dodano faktury");
 
 
-            executeQuery("INSERT INTO wystawione_faktury VALUES(1,2,112,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(1,3,22,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(1,4,53,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(1,1,31,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(2,1,33,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(2,2,44,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(2,4,55,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(3,1,11,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(3,2,22,0);");
-            executeQuery("INSERT INTO wystawione_faktury VALUES(3,3,33,0);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(1,2,112,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(1,3,22,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(1,4,53,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(1,1,31,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(2,1,33,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(2,2,44,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(2,4,55,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(3,1,11,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(3,2,22,0,1,1);");
+            executeQuery("INSERT INTO produkty_na_fakturach_vat VALUES(3,3,33,0,1,1);");
 
             System.out.println("Dodano wystawione Faktury");
 
 
     }
 
+    public void usuwanieTabelv2(){
 
-    public void usuwanieTabel(){
 
-
-            executeQuery("DROP TABLE wystawione_faktury;");
-            executeQuery("DROP TABLE faktury;");
-            executeQuery("DROP TABLE produkty;");
-            executeQuery("DROP TABLE kontrachenci;");
+        executeQuery("DROP TABLE produkty_na_fakturach_vat;");
+        executeQuery("DROP TABLE faktury;");
+        executeQuery("DROP TABLE towary;");
+        executeQuery("DROP TABLE kontrachenci;");
 //            executeQuery("DROP TABLE faktury;");
 
     }
-
-    public void tworzenieBazyDanychv2(){
-
-        String Query;
-
-
-        try{
-            Query = """
-                     CREATE TABLE kontrachenci (\s
-                     kontrachent_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     kontrachent_name VARCHAR(40),
-                     nip VARCHAR(15)
-                     );
-                    """;
-
-            executeQuery(Query);
-        }catch (RuntimeException e){
-            System.out.println("Tabela KONTRACHENCI JUZ ISTNIEJE");
-        }
-
-        try {
-            Query = """
-                    CREATE TABLE faktury (\s
-                     faktura_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     nr_faktury VARCHAR(15),
-                     data_wystawienia DATE,
-                     kontrachent_id INT,
-                     cenaBrutto FLOAT(2),
-                      FOREIGN KEY (kontrachent_id) REFERENCES kontrachenci(kontrachent_id) ON DELETE SET NULL                     
-                     );
-                            """;
-
-            executeQuery(Query);
-        }catch (RuntimeException e){
-            System.out.println("TABELA FAKTURY JUZ ISTNIEJE");
-        }
-
-
-
-
-        try {
-            Query = """ 
-                     CREATE TABLE produkty ( 
-                     produkt_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                     produkt_name VARCHAR(40),
-                     cena_brutto FLOAT(4)
-                     );
-                    """;
-
-            executeQuery(Query);
-        } catch (RuntimeException e){
-            System.out.println("Tabela produkty juz istnieje");
-        }
-
-
-        //TODO ZAPROJEKTOWAC BAZEDANYCH TAK ZE KAZDA FAKTURA MA JEDNEGO KONTRACHENTA (faktura i kontrachent ot primary key) I WIELE PRODUKTOW COS JAKBY TABLICA PRODUKTOW
-
-
-
-        try {
-            Query = """
-                    CREATE TABLE wystawione_faktury ( 
-                     faktura_id INT,
-                     produkt_id INT,
-                     ilosc_produktow INT,
-                     cena_Brutto FLOAT(4),
-                     PRIMARY KEY(faktura_id, produkt_id),
-                     FOREIGN KEY(faktura_id) REFERENCES faktury(faktura_id) ON DELETE CASCADE ON UPDATE CASCADE,
-                     FOREIGN KEY(produkt_id) REFERENCES produkty(produkt_id) ON DELETE CASCADE ON UPDATE CASCADE
-                     );
-                    """;
-            executeQuery(Query);
-        }catch (RuntimeException e){
-            e.printStackTrace();
-        }
-    }
-
-
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,64 +15,70 @@ public class Main {
         //TODO CZY OBIEKTY POWINNY BYC PRZECHOWYWANE W PAMIECI UZYTKOWEJ RPGRAU CZY W DB JEST OK?
 
 
+
         DatabaseInitialization databaseInitialization = new DatabaseInitialization();
-
-//        FakturaVat fakturaVat =FakturaVat.getFakturaPoZadanymIndex(1);
-//        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
-
-        //KOMENDA DO USOWANIA POPRZEDNIEJ BAZY DANYCH
-        databaseInitialization.usuwanieTabel();
-
-        //KOMENDY DO TWORZENIE I INICJALIZACJI BAZY DANYCH
-        databaseInitialization.tworzenieBazyDanychv2();
+//        databaseInitialization.usuwanieTabelv2();
+        databaseInitialization.tworzenieBazydanychV2();
         databaseInitialization.dodaniePrzykładowychdanych();
 
-        //USTWANIE CENN BRUTTO W WYSTAWIONYCH FAKTURACH
-        FakturaVat fakturaVat = FakturaVat.getByIndexFromDataBase(1);
-        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
-//        fakturaVat.setCenaBruttoFakturyWBazieDanych(fakturaVat);
-
-        fakturaVat = FakturaVat.getByIndexFromDataBase(2);
-        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
-//        fakturaVat.setCenaBruttoFakturyWBazieDanych(fakturaVat);
-
-        fakturaVat = FakturaVat.getByIndexFromDataBase(3);
-        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
-//        fakturaVat.setCenaBruttoFakturyWBazieDanych(fakturaVat);
-
-
-
-
-        // DODANIE PARU KONTRACHENTOW
-
-        LinkedList<Kontrachent> listaKontrachentow = new LinkedList<>();
-        listaKontrachentow.add(new Kontrachent("MIRBUDEX","1234567890"));
-        listaKontrachentow.add(new Kontrachent("Biedronka", "334512967"));
-        listaKontrachentow.add(new Kontrachent("Oracle", "815462967"));
-
-
-        // DODANIE PARU PRODUKTOW
-        LinkedList<Produkt> listaProduktow = new LinkedList<>();
-        listaProduktow.add(new Produkt("Mleko", 2.50f));
-        listaProduktow.add(new Produkt("Czekolada", 10.00f));
-        listaProduktow.add(new Produkt("Samochód", 10000.00f));
-
-        //DODANIE 10 NOWYCH FAKTUR
-        int liczbaFakturDoWprowadzenia = 10;
-        LocalDate dataWystawienia = LocalDate.of(2021,8,11);
-        String  nabywca =  new String("Mirbux");
-        int     cenaBrutto = 0;
-
-        ArrayList<FakturaVat> listaFaktur = new ArrayList<>(liczbaFakturDoWprowadzenia);
-        for(int i  =0; i< liczbaFakturDoWprowadzenia; i++) {
-            listaFaktur.add(new FakturaVat(dataWystawienia, listaKontrachentow.get(1)));
-        }
-
-
-        do{
-            Menu.wyswietlanieMenu(listaFaktur);
-            FakturaVat.setLiczbaFaktur(listaFaktur.size());
-        }while (true);
+//        DatabaseInitialization databaseInitialization = new DatabaseInitialization();
+//
+////        FakturaVat fakturaVat =FakturaVat.getFakturaPoZadanymIndex(1);
+////        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
+//
+//        //KOMENDA DO USOWANIA POPRZEDNIEJ BAZY DANYCH
+//        databaseInitialization.usuwanieTabel();
+//
+//        //KOMENDY DO TWORZENIE I INICJALIZACJI BAZY DANYCH
+//        databaseInitialization.tworzenieBazyDanychv2();
+//        databaseInitialization.dodaniePrzykładowychdanych();
+//
+//        //USTWANIE CENN BRUTTO W WYSTAWIONYCH FAKTURACH
+//        FakturaVat fakturaVat = FakturaVat.getByIndexFromDataBase(1);
+//        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
+////        fakturaVat.setCenaBruttoFakturyWBazieDanych(fakturaVat);
+//
+//        fakturaVat = FakturaVat.getByIndexFromDataBase(2);
+//        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
+////        fakturaVat.setCenaBruttoFakturyWBazieDanych(fakturaVat);
+//
+//        fakturaVat = FakturaVat.getByIndexFromDataBase(3);
+//        fakturaVat.setCenaBruttoWFakturWystawionych(fakturaVat);
+////        fakturaVat.setCenaBruttoFakturyWBazieDanych(fakturaVat);
+//
+//
+//
+//
+//        // DODANIE PARU KONTRACHENTOW
+//
+//        LinkedList<Kontrachent> listaKontrachentow = new LinkedList<>();
+//        listaKontrachentow.add(new Kontrachent("MIRBUDEX","1234567890"));
+//        listaKontrachentow.add(new Kontrachent("Biedronka", "334512967"));
+//        listaKontrachentow.add(new Kontrachent("Oracle", "815462967"));
+//
+//
+//        // DODANIE PARU PRODUKTOW
+//        LinkedList<Produkt> listaProduktow = new LinkedList<>();
+//        listaProduktow.add(new Produkt("Mleko", 2.50f));
+//        listaProduktow.add(new Produkt("Czekolada", 10.00f));
+//        listaProduktow.add(new Produkt("Samochód", 10000.00f));
+//
+//        //DODANIE 10 NOWYCH FAKTUR
+//        int liczbaFakturDoWprowadzenia = 10;
+//        LocalDate dataWystawienia = LocalDate.of(2021,8,11);
+//        String  nabywca =  new String("Mirbux");
+//        int     cenaBrutto = 0;
+//
+//        ArrayList<FakturaVat> listaFaktur = new ArrayList<>(liczbaFakturDoWprowadzenia);
+//        for(int i  =0; i< liczbaFakturDoWprowadzenia; i++) {
+//            listaFaktur.add(new FakturaVat(dataWystawienia, listaKontrachentow.get(1)));
+//        }
+//
+//
+//        do{
+//            Menu.wyswietlanieMenu(listaFaktur);
+//            FakturaVat.setLiczbaFaktur(listaFaktur.size());
+//        }while (true);
 
     }
 }
